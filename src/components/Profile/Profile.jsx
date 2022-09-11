15eba7110059efb60ed96458b067dc0e60df5636
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2022-09-11 21:49:32 +0300

    updated_final

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -43,9 +43,9 @@ Profile.propTypes = {
         avatar: PropTypes.string.isRequired,
         location: PropTypes.string.isRequired,
         stats: PropTypes.shape({
-            followers: PropTypes.number,
-            views: PropTypes.number,
-            likes: PropTypes.number
+            followers: PropTypes.number.isRequired,
+            views: PropTypes.number.isRequired,
+            likes: PropTypes.number.isRequired,
         }).isRequired,
         tag: PropTypes.string.isRequired,
         username: PropTypes.string.isRequired,
